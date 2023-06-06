e92658700844e4fd37f4a9baef2e1b3164823cd2
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2023-06-06 09:26:51 -0700

    feat(rave): add bare-bones RaveMark app

---
+++ apps/rave-mark/frontend/src/polyfills.ts
@@ -0,0 +1,9 @@
+/**
+ * Provides polyfills needed for this application and its dependencies.
+ */
+
+/* istanbul ignore file */
+import { Buffer } from 'buffer';
+
+globalThis.global = globalThis;
+globalThis.Buffer = Buffer;
