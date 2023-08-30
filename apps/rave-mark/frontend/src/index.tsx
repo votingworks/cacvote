25c75f92d5aab34139b1434fa6fef252f1a69a62
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2023-08-30 14:09:12 -0700

    chore: remove the `dev-dock`

--- apps/rave-mark/frontend/src/index.tsx
+++ apps/rave-mark/frontend/src/index.tsx
@@ -1,13 +1,5 @@
 import './polyfills';
-import React from 'react';
 import ReactDom from 'react-dom';
-import { DevDock } from '@votingworks/dev-dock-frontend';
 import { App } from './app';
 
-ReactDom.render(
-  <React.Fragment>
-    <App />
-    <DevDock />
-  </React.Fragment>,
-  document.getElementById('root') as HTMLElement
-);
+ReactDom.render(<App />, document.getElementById('root') as HTMLElement);
