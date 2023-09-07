2cd760c44839447287a6669739c9e1a15ce8e81a
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2023-09-07 10:27:45 -0700

    refactor(rave-mark): extract `PinPadModal`

---
+++ apps/rave-mark/frontend/src/components/pin_pad_modal.tsx
@@ -0,0 +1,101 @@
+import {
+  Button,
+  Modal,
+  NumberPad,
+  PinLength,
+  Text,
+  usePinEntry,
+} from '@votingworks/ui';
+import React from 'react';
+import styled from 'styled-components';
+
+const NumberPadWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+  font-size: 1em;
+  > div {
+    width: 400px;
+  }
+  *:focus {
+    outline: none;
+  }
+`;
+
+const EnteredCode = styled.div`
+  margin-top: 5px;
+  text-align: center;
+  font-family: monospace;
+  font-size: 1.5em;
+  font-weight: 600;
+`;
+
+export interface PinPadModalProps {
+  pinLength: PinLength;
+  primaryButtonLabel: string;
+  dismissButtonLabel: string;
+  onEnter: (pin: string) => void;
+  onDismiss: () => void;
+  title?: string;
+  disabled?: boolean;
+  error?: string;
+}
+
+export function PinPadModal({
+  pinLength,
+  primaryButtonLabel,
+  dismissButtonLabel,
+  onEnter,
+  onDismiss,
+  title = 'Enter Your PIN',
+  disabled,
+  error,
+}: PinPadModalProps): JSX.Element {
+  const pinEntry = usePinEntry({ pinLength });
+
+  function handleEnter() {
+    onEnter(pinEntry.current);
+  }
+
+  function dismissPinModal() {
+    pinEntry.reset();
+    onDismiss();
+  }
+
+  return (
+    <Modal
+      title={title}
+      onOverlayClick={dismissPinModal}
+      content={
+        <React.Fragment>
+          {error && <Text error>Error: {error}</Text>}
+          <EnteredCode>{pinEntry.display}</EnteredCode>
+          <NumberPadWrapper>
+            <NumberPad
+              disabled={disabled}
+              onButtonPress={pinEntry.handleDigit}
+              onBackspace={pinEntry.handleBackspace}
+              onClear={pinEntry.reset}
+              onEnter={handleEnter}
+            />
+          </NumberPadWrapper>
+        </React.Fragment>
+      }
+      actions={
+        <React.Fragment>
+          <Button
+            variant="primary"
+            onPress={handleEnter}
+            disabled={disabled}
+            aria-label="enter"
+          >
+            {primaryButtonLabel}
+          </Button>
+          <Button onPress={dismissPinModal} aria-label="cancel">
+            {dismissButtonLabel}
+          </Button>
+        </React.Fragment>
+      }
+    />
+  );
+}
--- apps/rave-mark/frontend/src/screens/voting/submit_screen.tsx
+++ apps/rave-mark/frontend/src/screens/voting/submit_screen.tsx
@@ -3,85 +3,50 @@ import {
   Button,
   H2,
   Main,
-  Modal,
-  NumberPad,
   P,
   Prose,
   Screen,
   fontSizeTheme,
-  usePinEntry,
 } from '@votingworks/ui';
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import { useState } from 'react';
 import { castBallot } from '../../api';
+import { PinPadModal } from '../../components/pin_pad_modal';
 import { COMMON_ACCESS_CARD_PIN_LENGTH } from '../../globals';
-
-const NumberPadWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 10px;
-  font-size: 1em;
-  > div {
-    width: 400px;
-  }
-  *:focus {
-    outline: none;
-  }
-`;
-
-const EnteredCode = styled.div`
-  margin-top: 5px;
-  text-align: center;
-  font-family: monospace;
-  font-size: 1.5em;
-  font-weight: 600;
-`;
 
 export interface SubmitScreenProps {
   votes: VotesDict;
   onSubmitted: () => void;
 }
 
-const pinLength = COMMON_ACCESS_CARD_PIN_LENGTH;
-
 export function SubmitScreen({
   votes,
   onSubmitted,
 }: SubmitScreenProps): JSX.Element {
-  const [error, setError] = useState<string>();
+  const [pinError, setPinError] = useState<string>();
   const [isShowingPinModal, setIsShowingPinModal] = useState(false);
-  const [isCheckingPin, setIsCheckingPin] = useState(false);
-  const pinEntry = usePinEntry({ pinLength });
   const castBallotMutation = castBallot.useMutation();
   const castBallotMutationMutateAsync = castBallotMutation.mutateAsync;
+  const isCheckingPin = castBallotMutation.isLoading;
 
   async function submitBallot(pin: string) {
-    setError(undefined);
-    setIsCheckingPin(true);
+    setPinError(undefined);
     try {
       await castBallotMutationMutateAsync(
         { votes, pin },
         {
           onError(err) {
-            setError(err instanceof Error ? err.message : `${err}`);
+            setPinError(err instanceof Error ? err.message : `${err}`);
           },
         }
       );
       onSubmitted();
     } catch (err) {
-      setError(err instanceof Error ? err.message : `${err}`);
-    } finally {
-      setIsCheckingPin(false);
+      setPinError(err instanceof Error ? err.message : `${err}`);
     }
   }
 
-  async function handleEnter() {
-    await submitBallot(pinEntry.current);
-  }
-
-  function dismissPinModal() {
-    setIsShowingPinModal(false);
-    pinEntry.reset();
+  async function handleEnter(pin: string) {
+    await submitBallot(pin);
   }
 
   return (
@@ -102,36 +67,16 @@ export function SubmitScreen({
             Cast My Ballot
           </Button>
           {isShowingPinModal && (
-            <Modal
-              title="Enter Your PIN"
-              onOverlayClick={dismissPinModal}
-              content={
-                <React.Fragment>
-                  {error && <P>{error}</P>}
-                  <EnteredCode>{pinEntry.display}</EnteredCode>
-                  <NumberPadWrapper>
-                    <NumberPad
-                      disabled={isCheckingPin}
-                      onButtonPress={pinEntry.handleDigit}
-                      onBackspace={pinEntry.handleBackspace}
-                      onClear={pinEntry.reset}
-                      onEnter={handleEnter}
-                    />
-                  </NumberPadWrapper>
-                </React.Fragment>
+            <PinPadModal
+              pinLength={COMMON_ACCESS_CARD_PIN_LENGTH}
+              primaryButtonLabel={
+                isCheckingPin ? 'Checking…' : 'Cast My Ballot'
               }
-              actions={
-                <React.Fragment>
-                  <Button
-                    variant="primary"
-                    onPress={handleEnter}
-                    disabled={isCheckingPin}
-                  >
-                    {isCheckingPin ? 'Checking…' : 'Cast My Ballot'}
-                  </Button>
-                  <Button onPress={dismissPinModal}>Go Back</Button>
-                </React.Fragment>
-              }
+              dismissButtonLabel="Go Back"
+              onEnter={handleEnter}
+              onDismiss={() => setIsShowingPinModal(false)}
+              disabled={isCheckingPin}
+              error={pinError}
             />
           )}
         </Prose>
