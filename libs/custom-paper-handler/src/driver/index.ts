36354e072f4f873df535c8e265a319f0cb499960
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2023-09-11 11:22:24 -0700

    chore: remove unused apps and packages

--- libs/custom-paper-handler/src/driver/index.ts
+++
@@ -1,7 +0,0 @@
-/* istanbul ignore file */
-export * from './driver';
-export * from './driver_interface';
-export * from './coders';
-export * from './constants';
-export * from './helpers';
-export * from './minimal_web_usb_device';
