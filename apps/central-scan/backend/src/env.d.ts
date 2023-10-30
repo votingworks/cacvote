e818ffb3ee694da5db6dcc868d1da967477f10e5
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2023-10-30 11:40:29 -0700

    chore: remove unused apps and packages

--- apps/central-scan/backend/src/env.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace NodeJS {
-  export interface ProcessEnv {
-    readonly CI?: string;
-    readonly NODE_ENV: 'development' | 'production' | 'test';
-    readonly PORT?: string;
-    readonly SCAN_WORKSPACE?: string;
-  }
-}
