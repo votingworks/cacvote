cb2a27fa426c0dd5a4ad2d00f77aeb7111a57976
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2023-11-15 15:54:41 -0800

    feat: remove unnecessary registration fields

--- apps/rave-mark/frontend/src/screens/registration/start_screen.tsx
+++ apps/rave-mark/frontend/src/screens/registration/start_screen.tsx
@@ -21,12 +21,6 @@ export function StartScreen(): JSX.Element {
   const [jurisdictionId, setJurisdictionId] = useState<ServerId>();
   const [givenName, setGivenName] = useState(cardDetails?.givenName ?? '');
   const [familyName, setFamilyName] = useState(cardDetails?.familyName ?? '');
-  const [addressLine1, setAddressLine1] = useState('');
-  const [addressLine2, setAddressLine2] = useState('');
-  const [city, setCity] = useState('');
-  const [state, setState] = useState('');
-  const [postalCode, setPostalCode] = useState('');
-  const [stateId, setStateId] = useState('');
   const [isShowingPinModal, setIsShowingPinModal] = useState(false);
   const createVoterRegistrationMutation = createVoterRegistration.useMutation();
   const error = createVoterRegistrationMutation.data?.err();
@@ -44,12 +38,6 @@ export function StartScreen(): JSX.Element {
       jurisdictionId: jxId,
       givenName,
       familyName,
-      addressLine1,
-      addressLine2,
-      city,
-      state,
-      postalCode,
-      stateId,
       pin,
     });
   }
@@ -74,62 +62,6 @@ export function StartScreen(): JSX.Element {
             value={familyName}
             onChange={(newValue) => {
               setFamilyName(newValue);
-            }}
-          />
-        </InlineForm>
-        <InlineForm>
-          <TextInput
-            label="Address Line 1"
-            data-testid="address-line-1"
-            value={addressLine1}
-            onChange={(newValue) => {
-              setAddressLine1(newValue);
-            }}
-          />
-        </InlineForm>
-        <InlineForm>
-          <TextInput
-            label="Address Line 2"
-            data-testid="address-line-2"
-            value={addressLine2}
-            onChange={(newValue) => {
-              setAddressLine2(newValue);
-            }}
-          />
-        </InlineForm>
-        <InlineForm>
-          <TextInput
-            label="City"
-            data-testid="city"
-            value={city}
-            onChange={(newValue) => {
-              setCity(newValue);
-            }}
-          />
-          <TextInput
-            label="State"
-            data-testid="state"
-            value={state}
-            onChange={(newValue) => {
-              setState(newValue);
-            }}
-          />
-          <TextInput
-            label="Postal Code"
-            data-testid="postal-code"
-            value={postalCode}
-            onChange={(newValue) => {
-              setPostalCode(newValue);
-            }}
-          />
-        </InlineForm>
-        <InlineForm>
-          <TextInput
-            label="State ID / Driver’s License ID"
-            data-testid="state-id"
-            value={stateId}
-            onChange={(newValue) => {
-              setStateId(newValue);
             }}
           />
           <Select value={jurisdictionId} onChange={onChangeJurisdictionId}>
