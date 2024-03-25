6370695a3f874d8375a6a69f85793db48b076e9e
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2024-03-25 11:14:45 -0700

    fix(jx): use the new `VxCard` to make it compile

--- apps/cacvote-jx-terminal/backend/src/smartcard.rs
+++ apps/cacvote-jx-terminal/backend/src/smartcard.rs
@@ -2,7 +2,8 @@ use std::fmt::Debug;
 use std::ops::Deref;
 use std::sync::{Arc, Mutex};
 
-use auth_rs::card_details::CardDetails;
+use auth_rs::card_details::CardDetailsWithAuthInfo;
+use auth_rs::vx_card::VxCard;
 use auth_rs::{CardReader, SharedCardReaders};
 use types_rs::cacvote::SmartcardStatus;
 
@@ -11,7 +12,7 @@ pub(crate) type DynStatusGetter = Arc<dyn StatusGetterTrait + Send + Sync>;
 #[cfg_attr(test, mockall::automock)]
 pub(crate) trait StatusGetterTrait {
     fn get(&self) -> SmartcardStatus;
-    fn get_card_details(&self) -> Option<CardDetails>;
+    fn get_card_details(&self) -> Option<CardDetailsWithAuthInfo>;
 }
 
 /// Provides access to the current smartcard status.
@@ -22,7 +23,8 @@ pub(crate) struct StatusGetter {
     #[allow(dead_code)]
     readers_with_cards: SharedCardReaders,
     #[allow(dead_code)]
-    last_selected_card_reader_info: Arc<Mutex<Option<(String, CardDetails)>>>,
+    last_selected_card_reader_info: Arc<Mutex<Option<(String, CardDetailsWithAuthInfo)>>>,
+    card: Option<Arc<VxCard>>,
 }
 
 impl Debug for StatusGetter {
@@ -41,6 +43,45 @@ impl StatusGetter {
             ctx: pcsc::Context::establish(pcsc::Scope::User).unwrap(),
             readers_with_cards,
             last_selected_card_reader_info: Arc::new(Mutex::new(None)),
+            card: None,
+        }
+    }
+
+    pub(crate) fn refresh_card_details(&mut self) {
+        let readers = self.readers_with_cards.lock().unwrap();
+        let Some(reader_name) = readers.first() else {
+            return;
+        };
+
+        let mut cached_card_details = self.last_selected_card_reader_info.lock().unwrap();
+        if let Some((ref name, ref details)) = cached_card_details.deref() {
+            let same_reader_has_card = readers.iter().any(|reader| reader == name);
+
+            if same_reader_has_card {
+                return;
+            }
+        }
+
+        let reader = CardReader::new(self.ctx.clone(), reader_name.clone());
+        match reader.get_card() {
+            Ok(card) => {
+                self.card = Some(Arc::new(card));
+            }
+            Err(e) => {
+                tracing::error!("error getting card: {e}");
+            }
+        }
+
+        if let Some(card) = self.card.as_ref() {
+            match card.read_card_details() {
+                Ok(card_details_with_auth_info) => {
+                    *cached_card_details =
+                        Some((reader_name.to_string(), card_details_with_auth_info));
+                }
+                Err(e) => {
+                    tracing::error!("error reading card details: {e}");
+                }
+            }
         }
     }
 }
@@ -61,29 +102,12 @@ impl StatusGetterTrait for StatusGetter {
 
     #[allow(dead_code)]
     #[must_use]
-    fn get_card_details(&self) -> Option<CardDetails> {
-        let readers = self.readers_with_cards.lock().unwrap();
-        let reader_name = readers.first()?;
-
-        let mut cached_card_details = self.last_selected_card_reader_info.lock().unwrap();
-        if let Some((ref name, ref details)) = cached_card_details.deref() {
-            let same_reader_has_card = readers.iter().any(|reader| reader == name);
-
-            if same_reader_has_card {
-                return Some(details.clone());
-            }
-        }
-
-        let reader = CardReader::new(self.ctx.clone(), reader_name.clone());
-        let card_details = match reader.read_card_details() {
-            Ok(card_details) => card_details,
-            Err(e) => {
-                tracing::error!("error reading card details: {e}");
-                return None;
-            }
+    fn get_card_details(&self) -> Option<CardDetailsWithAuthInfo> {
+        self.refresh_card_details();
+        let Ok(cached_card_details) = self.last_selected_card_reader_info.lock() else {
+            return None;
         };
 
-        *cached_card_details = Some((reader_name.to_string(), card_details));
-        cached_card_details.clone().map(|(_, details)| details)
+        cached_card_details.map(|(_, details)| details)
     }
 }
