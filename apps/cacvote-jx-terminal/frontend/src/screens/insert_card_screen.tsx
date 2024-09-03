2f0f6db24c83c5a578f0f04eaa79b67b09072dca
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2024-09-03 13:14:11 -0700

    feat(jx): when a card is inserted but not auth'd, show "Invalid Card"

--- apps/cacvote-jx-terminal/frontend/src/screens/insert_card_screen.tsx
+++ apps/cacvote-jx-terminal/frontend/src/screens/insert_card_screen.tsx
@@ -8,7 +8,10 @@ import {
   Screen,
 } from '@votingworks/ui';
 import * as api from '../api';
-import { AuthenticatedSessionData } from '../cacvote-server/session_data';
+import {
+  AuthenticatedSessionData,
+  UnauthenticatedSessionData,
+} from '../cacvote-server/session_data';
 
 export function InsertCardScreen(): JSX.Element {
   const sessionDataQuery = api.sessionData.useQuery();
@@ -28,7 +31,12 @@ export function InsertCardScreen(): JSX.Element {
       <Main centerChild>
         <FullScreenIconWrapper>
           <InsertCardImage />
-          <H1 align="center">Insert Your Card</H1>
+          <H1 align="center">
+            {sessionData instanceof UnauthenticatedSessionData &&
+            sessionData.getHasSmartcard()
+              ? 'Invalid Card'
+              : 'Insert Your Card'}
+          </H1>
         </FullScreenIconWrapper>
       </Main>
     </Screen>
