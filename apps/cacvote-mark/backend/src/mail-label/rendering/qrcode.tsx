434426743de8be8f625297d63d91fca68c769f4f
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2025-04-03 11:30:12 -0700

    fix(mark): increase size of label address & QR code (#188)
    This change redoes the rendering to use HTML+flexbox instead of SVG
    except for the QR code, which I also reworked to use a single `<path>`
    element which avoids the small gaps between modules that using `<rect>`
    produced.

--- apps/cacvote-mark/backend/src/mail-label/rendering/qrcode.tsx
+++ apps/cacvote-mark/backend/src/mail-label/rendering/qrcode.tsx
@@ -1,6 +1,11 @@
-import { integers } from '@votingworks/basics';
 import React from 'react';
 import { qrcodegen } from './qrcodegen';
+
+/**
+ * The Nippon thermal label printer can print 8 dots per millimeter, and
+ * https://www.qrcode.com/en/howto/cell.html recommends using 0.5mm for that density.
+ */
+const MIN_MODULE_SIZE_MM = 0.5;
 
 export function QrCode({ data }: { data: Uint8Array }): JSX.Element {
   const encoded = qrcodegen.QrCode.encodeBinary(
@@ -8,31 +13,31 @@ export function QrCode({ data }: { data: Uint8Array }): JSX.Element {
     qrcodegen.QrCode.Ecc.MEDIUM
   );
 
-  const constrainingSize = 87.5;
-  const moduleSize = 20;
-  const scale = constrainingSize / encoded.size / moduleSize;
+  const goOrigin = 'M 0 0 ';
+  const drawSquare = 'h 1 v 1 h -1 z ';
+  const goNextColumn = 'm 1 0 ';
+  const goNextRow = `m ${-encoded.size} 1 `;
+
+  let d = goOrigin;
+  for (let y = 0; y < encoded.size; y += 1) {
+    for (let x = 0; x < encoded.size; x += 1) {
+      if (encoded.getModule(x, y)) {
+        d += drawSquare;
+      }
+      d += goNextColumn;
+    }
+    d += goNextRow;
+  }
 
   return (
-    <svg width={100} height={87.5}>
-      {[
-        ...integers()
-          .take(encoded.size)
-          .flatMap((y) =>
-            integers()
-              .take(encoded.size)
-              .map((x) => (
-                <rect
-                  key={`${x},${y}`}
-                  x={x * moduleSize}
-                  y={y * moduleSize}
-                  width={moduleSize}
-                  height={moduleSize}
-                  fill={encoded.getModule(x, y) ? 'black' : 'white'}
-                  transform={`scale(${scale})`}
-                />
-              ))
-          ),
-      ]}
+    <svg
+      // set the viewBox to the module size of the QR code
+      viewBox={`0 0 ${encoded.size} ${encoded.size}`}
+      // set the width and height to the desired physical size of the QR code in millimeters
+      width={`${encoded.size * MIN_MODULE_SIZE_MM}mm`}
+      height={`${encoded.size * MIN_MODULE_SIZE_MM}mm`}
+    >
+      <path d={d} fill="black" />
     </svg>
   );
 }
